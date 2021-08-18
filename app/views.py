a82d8cd51a5e6e85d9da8febbe3d584cfed85e58
likithnani <likithnani567@gmail.com>
2021-08-18 17:36:36 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def app_hai(request):
+    return render(request,'app_hai.html')
+
+def app_string(request):
+    return HttpResponse('app_string')
